6786b2134aa612f5627cc78025e30aa248b2b38b
Jainam <jainamoswal1811@gmail.com>
2025-08-02 10:08:08 +0530

    added auth routes, tested & documented them, added helmet & user input validator for security

--- server/server.js
+++ server/server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import "dotenv/config";
-import { rateLimit } from "express-rate-limit"
+import { rateLimit } from "express-rate-limit";
+import authRoutes from "./routes/auth.js";
+import helmet from "helmet"
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json({limit: "10mb"}));
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet()); // Security middleware to set various HTTP headers
+
+// Routes
+app.use("/api/auth", authRoutes);
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
